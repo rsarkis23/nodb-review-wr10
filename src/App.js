10b5a3f187b828338ce7485e46e077fb513a56f5
mattcbodily <mattcbodily@gmail.com>
2021-03-11 08:54:43 -0700

    boilerplate reset

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import React, {Component} from 'react';
 import Header from './Components/Header';
 import Finder from './Components/Finder';
 import Pokedex from './Components/Pokedex';
-import axios from 'axios';
 import './App.css';
 
 class App extends Component {
@@ -27,22 +26,14 @@ class App extends Component {
     //componentDidMount is a lifecycle method. It will fire after the first invocation of render
     //in the component. This is a great method for retrieving data from a server that you need 
     //in your component right away.
-    axios.get('/api/caught-pokemon')
-      .then(res => {
-        this.setState({caughtPokemon: res.data})
-      })
-      .catch(err => console.log(err));
+
   }
 
   //This method will send a pokemon object to the server to be added to our API. To see
   //where this connects, view catchPokemon in server/pokemonCtrl.js, and to see where it
   //is invoked, visit Grass.js(referred to as this.props.catchFn)
   catchPokemon(pokemon){
-    axios.post('/api/caught-pokemon', {pokemon: pokemon})
-      .then(res => {
-        this.setState({caughtPokemon: res.data});
-      })
-      .catch(err => console.log(err));
+
   }
 
   //This method will update the name of a caughtPokemon. It takes id and newName as arguments,
@@ -52,22 +43,13 @@ class App extends Component {
   editName = (id, newName) => {
     let body = {name: newName};
 
-    axios.put(`/api/caught-pokemon/${id}`, body)
-      .then(res => {
-        this.setState({caughtPokemon: res.data});
-      })
-      .catch(err => console.log(err));
   }
 
   //This method will remove a pokemon from our API. It takes an id to use as a parameter of our
   //axios request. To see where this connects, view releasePokemon in pokemonCtrl.js, and to see
   //where it is invoked, visit Pokemon.js(referred to as releaseFn)
   releasePokemon = (id) => {
-    axios.delete(`/api/caught-pokemon/${id}`)
-      .then(res => {
-        this.setState({caughtPokemon: res.data});
-      })
-      .catch(err => console.log(err));
+    
   }
 
   render(){
